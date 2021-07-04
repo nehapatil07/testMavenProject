7efabbfa5baf9af3b046ac3d492b902a4c202219
Neha <Neha@Neha-PC>
2021-07-04 18:30:59 +0530

    JBK simple test case

---
+++ JBKsite/src/test/java/com/jbk/ass/NewTest.java
@@ -0,0 +1,23 @@
+package com.jbk.ass;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	static WebDriver driver;
+  @Test
+  public void f() {
+	  System.setProperty("webdriver.driver.chrome", "Chromedriver.exe");
+	  driver=new ChromeDriver();
+	  driver.get("file:///D:/Offline%20Website/Offline%20Website/index.html");
+	  driver.findElement(By.id("email")).sendKeys("kiran@gmail.com");
+	  driver.findElement(By.id("password")).sendKeys("123456");
+	  driver.findElement(By.xpath("//*[@id=\"form\"]/div[3]/div/button")).click();
+	  String ActTitle=driver.getTitle();
+	  System.out.println(ActTitle);
+	  Assert.assertEquals(ActTitle, "JavaByKiran | Dashboard");
+  }
+}
